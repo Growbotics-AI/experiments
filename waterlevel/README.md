4a6e3f01c87f218e70c884033948c31135e3683e
Tomas <tomas@growbotics.ai>
2018-07-30 20:59:44 +0000

    Update readme file

--- waterlevel/README.md
+++ waterlevel/README.md
@@ -4,3 +4,6 @@ The code relies on Adafruit Python MCP3008, it can be installed this way:
 sudo pip install adafruit-mcp3008
 ```
 
+More details available here:
+
+https://community.growbotics.ai/t/testing-water-level-sensor/94
--- waterlevel/waterlevel.py
+++ waterlevel/waterlevel.py
@@ -1,12 +1,6 @@
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-
-CLK  = 11
-MISO = 9
-MOSI = 10
-CS   = 8
-
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+mcp = Adafruit_MCP3008.MCP3008(clk=11, cs=8, miso=9, mosi=10)
 
 print(mcp.read_adc(0))
